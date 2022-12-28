67e07ac26dcec227eaae9774783a50db007370c4
Lori <fallenlorelei@hotmail.com>
2022-12-28 11:57:17 -0500

    jump state added

--- player/MagicalGirl.gd
+++ player/MagicalGirl.gd
@@ -2,18 +2,18 @@ extends KinematicBody2D
 
 export var ACCELERATION = 500
 export var MAX_SPEED = 80
-export var ROLL_SPEED = 120
 export var FRICTION = 500
+export var JUMP_SPEED = 150
 
 enum {
 	MOVE,
-	ROLL,
+	JUMP,
 	ATTACK
 }
 
 var state = MOVE
 var velocity = Vector2.ZERO
-#var roll_vector = Vector2.DOWN
+var jump_vector = Vector2.DOWN
 
 onready var animationPlayer = $"%AnimationPlayer"
 onready var animationTree = $"%AnimationTree"
@@ -22,7 +22,7 @@ onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
 	animationTree.active = true
-#	swordHitbox.knockback_vector = roll_vector
+#	swordHitbox.knockback_vector = jump_vector
 
 func _process(_delta):
 	pass
@@ -31,8 +31,8 @@ func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
-#		ROLL:
-#			roll_state(delta)
+		JUMP:
+			jump_state(delta)
 #		ATTACK:
 #			attack_state(delta)
 	
@@ -44,12 +44,12 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-#		roll_vector = input_vector
+		jump_vector = input_vector
 #		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 #		animationTree.set("parameters/Attack/blend_position", input_vector)
-#		animationTree.set("parameters/Roll/blend_position", input_vector)
+		animationTree.set("parameters/Jump/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	
@@ -59,16 +59,16 @@ func move_state(delta):
 		
 	move()
 	
-#	if Input.is_action_just_pressed("roll"):
-#		state = ROLL
+	if Input.is_action_just_pressed("jump"):
+		state = JUMP
 #
 #	if Input.is_action_just_pressed("attack"):
 #		state = ATTACK
 
-#func roll_state(delta):
-#	velocity = roll_vector * ROLL_SPEED
-#	animationState.travel("Roll")
-#	move()
+func jump_state(delta):
+	velocity = jump_vector * JUMP_SPEED
+	animationState.travel("Jump")
+	move()
 #
 #func attack_state(delta):
 #	velocity = Vector2.ZERO
@@ -77,9 +77,9 @@ func move_state(delta):
 func move():
 	velocity = move_and_slide(velocity)
 
-#func roll_animation_finished():
-#	velocity = Vector2.ZERO
-#	state = MOVE
+func jump_animation_finished():
+	velocity = Vector2.ZERO
+	state = MOVE
 #
 #func attack_animation_finished():
 #	state = MOVE
