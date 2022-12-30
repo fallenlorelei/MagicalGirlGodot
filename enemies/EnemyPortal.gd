1f70a0503fcc9c144ebe14927ac73f59a806a31f
Lori <fallenlorelei@hotmail.com>
2022-12-30 14:48:02 -0500

    portal random enemy dictionary

--- enemies/EnemyPortal.gd
+++ enemies/EnemyPortal.gd
@@ -1,19 +1,32 @@
 extends Node2D
 
-
-
 onready var animationPlayer = $AnimationPlayer
 onready var timer = $Timer
 onready var sprite = $Sprite
 
-onready var Slime = load("res://enemies/Slime.tscn")
+var randomEnemy
+
+
+#export(String,"Slime","BiggerSlime") var enemy_list
+
+
+var enemy_dict = {
+	"Slime": load("res://enemies/Slime.tscn"),
+	"MagicalGirl": load("res://player/MagicalGirl.tscn")
+}
+
 
 export var spawnAmount = 1
 
-
 func _ready():
-	print("portal is", global_position)
+#	enemy_list = "Slime"
+	randomEnemy = get_random_enemy()
+	print(randomEnemy)
 
+func get_random_enemy():
+	var random_id = randi() % enemy_dict.size()
+	var random_value = enemy_dict.values()[random_id]
+	return random_value
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "opening":
@@ -23,13 +36,13 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 		sprite.hide()
 
 func spawn_enemies():
-	var slimeSpawn = Slime.instance()
-	slimeSpawn.global_position = global_position
-	get_parent().add_child(slimeSpawn)
+	var enemySpawn = randomEnemy.instance()
+	enemySpawn.global_position = global_position
+	get_parent().add_child(enemySpawn)
 	spawnAmount -= 1
 	if spawnAmount >= 0:
 		timer.start()
-	
+
 
 func _on_Timer_timeout():
 	if spawnAmount <= 0:
