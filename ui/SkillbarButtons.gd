b7647add6ba0c17d55a23e54e932e250903f9070
Lori <fallenlorelei@hotmail.com>
2023-01-12 10:56:49 -0500

    skillbar tooltip fix yay

--- ui/SkillbarButtons.gd
+++ ui/SkillbarButtons.gd
@@ -2,16 +2,13 @@ extends TextureButton
 
 onready var tooltip = preload("res://ui/Tooltip.tscn")
 
-var skill_slot
-var skill_name
 
 func _ready():
 	pass
 
-
 func get_drag_data(_pos):
-	skill_slot = get_parent().get_name()
-	skill_name = get_owner().loaded_skills[skill_slot]
+	var skill_slot = get_parent().get_name()
+	var skill_name = get_owner().loaded_skills[skill_slot]
 	print("dragging ", skill_slot, ": ", skill_name)
 	if skill_name != null:
 		var data = {}
@@ -20,6 +17,7 @@ func get_drag_data(_pos):
 		data["origin_skill_name"] = skill_name
 		data["origin_skill_slot"] = skill_slot
 		
+		# Dragging texture
 		var drag_texture = TextureRect.new()
 		drag_texture.expand = true
 		drag_texture.texture = texture_normal
@@ -37,40 +35,52 @@ func get_drag_data(_pos):
 func can_drop_data(_pos, data):
 	var target_skill_slot_scene = get_parent()
 	if target_skill_slot_scene.is_in_group("DraggableSkill"):
+		
 		var target_skill_slot = get_parent().get_name()
+		var target_skill_name = get_owner().loaded_skills[target_skill_slot]
+
 		if get_owner().loaded_skills[target_skill_slot] == null:
 			data["target_texture"] = null
 			data["target_skill_name"] = null
+			data["target_skill_slot"] = null
+			
 		else:
-			data["target_skill_name"] = get_owner().loaded_skills[target_skill_slot]
 			data["target_texture"] = texture_normal
+			data["target_skill_name"] = target_skill_name
+			data["target_skill_slot"] = target_skill_slot
+			
+		
 		return true
 	else:
 		return false
 	
 func drop_data(_pos, data):
+	if has_node("Tooltip") and get_node("Tooltip").valid:
+		get_node("Tooltip").free()
+		
 	var target_skill_slot = get_parent().get_name()
-	var origin_slot = data["origin_node"].get_parent().get_name()
+	var origin_skill_slot = data["origin_node"].get_parent().get_name()
 	
 	#Update data of origin
-	get_owner().loaded_skills[origin_slot] = data["target_skill_name"]
+	get_owner().loaded_skills[origin_skill_slot] = data["target_skill_name"]
 	
-	#Update texture of the origin
+	#Update texture of orign
 	if data["target_skill_name"] == null:
-		var empty_texture = null
-		data["origin_node"].texture_normal = empty_texture
+		data["origin_node"].texture_normal = null
 	else:
 		data["origin_node"].texture_normal = data["target_texture"]
 		
-	#Update texture and data of target
+	#Update both data and texture of target
 	get_owner().loaded_skills[target_skill_slot] = data["origin_skill_name"]
 	texture_normal = data["origin_texture"]
-
+	
 
 func _on_TextureButton_mouse_entered():
 	var tooltip_instance = tooltip.instance()
+	var skill_slot = get_parent().get_name()
+	var skill_name = get_owner().loaded_skills[skill_slot]
 	tooltip_instance.origin = "Skillbar"
-	tooltip_instance.skillslot = get_parent().get_name()
+	tooltip_instance.skillslot = skill_slot
 	tooltip_instance.skill_name = skill_name
 	
 	tooltip_instance.rect_position = get_parent().get_global_transform_with_canvas().origin - Vector2(40,80)
@@ -81,4 +91,5 @@ func _on_TextureButton_mouse_entered():
 		get_node("Tooltip").show()
 
 func _on_TextureButton_mouse_exited():
-	get_node("Tooltip").free()
+	if has_node("Tooltip") and get_node("Tooltip").valid:
+		get_node("Tooltip").free()
--- ui/Tooltip.gd
+++ ui/Tooltip.gd
@@ -2,15 +2,29 @@ extends Popup
 
 #onready var skillbar = get_node("../../CanvasLayer/SkillBar")
 
+onready var skillNameLabel = $Background/MarginContainer/VBoxContainer/SkillName
+onready var skillTypeLabel = $Background/MarginContainer/VBoxContainer/SkillType
+onready var damageOrHealLabel = $Background/MarginContainer/VBoxContainer/HBoxContainer/DamageOrHeal
+onready var amountLabel = $Background/MarginContainer/VBoxContainer/HBoxContainer/DamageHealAmount
+onready var skillDescriptionLabel = $Background/MarginContainer/VBoxContainer/SkillDescription
+
 var origin = ""
 var skillslot = ""
 var skill_name = ""
 var valid = false
 
 func _ready():
-	var skill_name
-	if origin == "Skillbar":
+	if origin == "Skillbar" and skill_name != null:
 		valid = true
 	
 	if valid:
-		pass
+		skillNameLabel.set_text(skill_name.capitalize())
+		
+		skillTypeLabel.set_text(DataImport.skill_data[skill_name].SkillType.capitalize())
+		
+		if DataImport.skill_data[skill_name].CanDamage == true:
+			damageOrHealLabel.set_text("Damage Amount: ")
+			amountLabel.set_text(str(DataImport.skill_data[skill_name].SkillDamage))
+		elif DataImport.skill_data[skill_name].CanHeal == true:
+			damageOrHealLabel.set_text("Heal Amount: ")
+			amountLabel.set_text(str(DataImport.skill_data[skill_name].HealAmount))
