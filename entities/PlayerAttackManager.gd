9b2161270914c09c8f481a5461a2c7a1cbe928a2
Lori <fallenlorelei@hotmail.com>
2023-01-17 15:11:54 -0500

    line on projectile

--- entities/PlayerAttackManager.gd
+++ entities/PlayerAttackManager.gd
@@ -4,6 +4,7 @@ onready var attackAnimationTimer = $AttackAnimationTimer
 onready var globalCooldown = $GlobalCooldown
 onready var castingCircleSprite = $CastingCircle
 onready var castingCircleAnimation = $CastingCircle/AnimationPlayer
+onready var projectileLine = $Line2D
 
 var parentCursorDirection
 var parentCursorLocation
@@ -13,7 +14,7 @@ var skillElement
 var distance
 var outOfBounds = false
 var castingCircleRotatingPosition
-
+			
 func _ready():
 	castingCircleRotatingPosition = 0.0
 
@@ -21,17 +22,25 @@ func _physics_process(_delta):
 	parentCursorDirection = get_parent().cursorDirection
 	parentCursorLocation = get_parent().cursorLocation
 	
-	
+	var cursorLocation = get_local_mouse_position()
+	var cursorDirection = (get_global_mouse_position() - position).normalized()
 
 	if skillName != null:
 		get_parent().animationTree.set("parameters/Attack/blend_position", parentCursorDirection)
 		get_parent().animationTree.set("parameters/Idle/blend_position", parentCursorDirection)
 		get_parent().animationTree.set("parameters/Run/blend_position", parentCursorDirection)
 		
+		# Move casting circle/projectile preview
 		if DataImport.skill_data[skillName].SkillType == "at_cursor":
 			castingCircleSprite.global_position = parentCursorLocation
+			
 		if DataImport.skill_data[skillName].SkillType == "around_self":
 			castingCircleSprite.global_position = global_position + Vector2(0,6)
+			
+		if DataImport.skill_data[skillName].SkillType == "projectile":	
+			projectileLine.set_point_position(0,Vector2(0,0))
+			var length = parentCursorDirection * distance
+			projectileLine.set_point_position(1,length)
 		
 		# Some abilities can still be cast if they are out of bounds
 		var canCastOutOfBounds
@@ -64,13 +73,9 @@ func _physics_process(_delta):
 				stop_casting()
 			else:
 				release_ability()
-
-func stop_casting():
-	get_parent().state = 0
-	var TW = get_tree().create_tween()
-	TW.tween_property(castingCircleSprite, "scale", Vector2(0,0), .2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	skillName = null
-	
+		
+		if skillShortcut == "left_click":
+			release_ability()
 
 func check_global_cooldown():
 	return globalCooldown.is_stopped()
@@ -86,21 +91,21 @@ func start_ability(selected_skill, selected_shortcut):
 		#Showing casting circle if relevant
 		match DataImport.skill_data[skillName].SkillType:
 			"at_cursor":
-				show_casting_circle(1)
+				show_casting(1, "at_cursor")
 			"self_utility":
 				pass
 			"around_self":
-				show_casting_circle(-1)
+				show_casting(-1, "around_self")
 			"front_arc":
 				pass
 			"projectile":
-				pass
+				show_casting(-1, "projectile")
 
-func show_casting_circle(zindex):
+func show_casting(zindex, type):
 	#Resize casting circle to size of collision
 	var radiusSize = DataImport.skill_data[skillName].SkillRadius
-
-	if radiusSize != null:
+	
+	if type == "at_cursor" or type == "around_self":
 		var sizeto = Vector2(radiusSize,radiusSize)
 		var size = castingCircleSprite.texture.get_size()
 		var scale_factor = sizeto/size * 2
@@ -111,6 +116,11 @@ func show_casting_circle(zindex):
 		var TW = get_tree().create_tween()
 		TW.tween_property(castingCircleSprite, "scale", scale_factor, .2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 		castingCircleSprite.show()
+	
+	if type == "projectile":
+		projectileLine.width = radiusSize * 2
+		projectileLine.z_index = zindex
+		projectileLine.show()
 
 func release_ability():
 	skillElement = DataImport.skill_data[skillName].Element
@@ -152,11 +162,15 @@ func release_ability():
 			get_tree().get_current_scene().add_child(ability)
 			ability.projectile()
 
+	stop_casting()
+
+func stop_casting():
+	get_parent().state = 0
 	var TW = get_tree().create_tween()
 	TW.tween_property(castingCircleSprite, "scale", Vector2(0,0), .2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	skillShortcut = null
+	projectileLine.hide()
 	skillName = null
-
+	
 func load_ability(skillName):
 	if skillName != null:
 		var path = "res://abilities/" + skillElement + "/" + skillName + "/" + skillName + ".tscn"
