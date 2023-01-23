dcde94fa5f2a6b907ff58938433a32ed9a9d623e
Lori <fallenlorelei@hotmail.com>
2023-01-23 01:21:19 -0500

    portal spawn fix

--- maze/rooms/Rooms.gd
+++ maze/rooms/Rooms.gd
@@ -4,8 +4,8 @@ const enemyPortal = preload("res://enemies/EnemyPortal.tscn")
 
 func _ready():
 	yield(get_tree(), "idle_frame")
-	var first_tile = get_parent().first_tile * 640
-	if global_position.x != first_tile.x:
+	var first_tile = get_parent().first_tile * Vector2(640,640)
+	if global_position != first_tile:
 		var portalSpawn = enemyPortal.instance()
 		add_child(portalSpawn)
 		portalSpawn.position = Vector2(340,280)
